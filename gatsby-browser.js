dfaeb72031e83facb8da4183116f03d205f6cd3f
Antonio Almena <aalmena1@gmail.com>
2021-06-25 16:11:07 -0700

    [9ntonio][1.5.1.b]

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/prefer-default-export */
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    "You're in Luck,there's an updated! Please reload and thanks for your support",
+    'You\'re in Luck,there\'s an updated! Please reload and thanks for your support',
   );
 
   if (answer === true) {
